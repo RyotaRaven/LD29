d47de8144f51d1c7f8be6f54ca588341c4e1f16e
RyotaRaven <Rachael.metcalf@utexas.edu>
2014-04-27 07:53:19 -0500

    Working basic game builds

--- LudamDare29/Assets/Scripts/PauseMenu.js
+++ LudamDare29/Assets/Scripts/PauseMenu.js
@@ -1,34 +1,46 @@
 ﻿var gamePaused : boolean = false;
- 
+function Start()
+{
+}
 function Update () {
+
+ if(!gamePaused)
+ {
+	Screen.lockCursor = true;
+	Screen.lockCursor = false;
+ }
  
  if(Input.GetKeyDown(KeyCode.Escape)){
  if(gamePaused){
  
  Time.timeScale=1.0;
  gamePaused = false;
+ Screen.lockCursor=true;
  
  }else{
  
  Time.timeScale = 0.0;
  gamePaused = true;
+ Screen.lockCursor=false;
  }
  }
  
 }
  
 function OnGUI(){
- 
  if(gamePaused){
  if(GUI.Button(Rect (Screen.width/2-50, 100, 100, 30), "Resume")){
  Time.timeScale = 1.0;
  gamePaused = false;
  }
- if(GUI.Button(Rect(Screen.width/2-50, 140, 100, 30), "Main Menu")){
+ if(GUI.Button(Rect(Screen.width/2-50, 140, 100, 30), "Main Menu"))
+ {
+ 	gamePaused=false;
 	Application.LoadLevel("Menu");
  }
  if(GUI.Button(Rect(Screen.width/2-50, 180, 100, 30), "Restart"))
  {
+ gamePaused=false;
  Time.timeScale = 1.0;
  Application.LoadLevel("World");
  }
@@ -36,4 +48,5 @@ function OnGUI(){
  Application.Quit();
     }
  }
+ else GUI.Label(Rect(0, 10, 150, 30), "Press Escape to Pause");
 }
---
+++ LudamDare29/Assets/Scripts/Win.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Win : MonoBehaviour {
+	
+	void OnTriggerEnter(Collider blah)
+	{
+		Application.LoadLevel ("Win");
+	}
+
+}
